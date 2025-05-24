67e081995480be678d681aec39969a49bc326d57
malken21 <77374813+malken21@users.noreply.github.com>
2025-05-25 07:46:47 +0900

    Update get-rss.rb

--- get-rss.rb
+++ get-rss.rb
@@ -8,6 +8,12 @@ def get_users
   File.open("users.json", "r") do |f|
     JSON.load(f)
   end
+rescue Errno::ENOENT
+  puts "Error: users.json not found."
+  return []
+rescue JSON::ParserError
+  puts "Error: Invalid JSON format in users.json."
+  return []
 end
 
 def replace_http_with_https(text)
@@ -16,10 +22,23 @@ end
 
 def download(url, path)
   FileUtils.mkdir_p(File.dirname(path))
-  content = URI.open(url).read
-  replaced_content = replace_http_with_https(content)
-  File.open(path, "w") do |local_file|
-    local_file.write(replaced_content)
+  begin
+    content = URI.open(url).read
+    replaced_content = replace_http_with_https(content)
+    File.open(path, "w") do |local_file|
+      local_file.write(replaced_content)
+    end
+    puts "Successfully downloaded: #{url} to #{path}"
+    return true
+  rescue OpenURI::HTTPError => e
+    puts "Error downloading #{url}: HTTP error - #{e.message}"
+    return false
+  rescue SocketError => e
+    puts "Error downloading #{url}: Network error - #{e.message}"
+    return false
+  rescue StandardError => e
+    puts "Error downloading #{url}: An unexpected error occurred - #{e.message}"
+    return false
   end
 end
 
@@ -27,15 +46,19 @@ def saveREADME(path, text)
   File.open(path, "w") do |file|
     file.puts(text)
   end
+  puts "README.md saved to #{path}"
 end
 
 if __FILE__ == $0
   usersText = ""
+  users = get_users()
 
-  get_users.each do |item|
-    download("http://#{Address}/#{item}/rss", "dist/#{item}.rss")
-    usersText << "## [#{item}](./#{item}.rss)\n"
+  users.each do |item|
+    if download("http://#{Address}/#{item}/rss", "dist/#{item}.rss")
+      usersText << "## [#{item}](./#{item}.rss)\n"
+    end
   end
 
+  FileUtils.mkdir_p("dist") unless Dir.exist?("dist")
   saveREADME("dist/README.md", usersText)
 end
